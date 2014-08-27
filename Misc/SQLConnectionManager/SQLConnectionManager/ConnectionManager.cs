6e994edaed8334d9d6ea0082dae4c0add84daea9
smishra <siddharth.mishra@beckman.com>
2014-08-27 16:14:24 -0400

    Implemented Load data from sql server through windows services

---
+++ Misc/SQLConnectionManager/SQLConnectionManager/ConnectionManager.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Windows.Forms;
+using SQLConnectionManager.Properties;
+
+namespace SQLConnectionManager
+{
+  public partial class ConnectionManager : Form
+  {
+    private string _connectionString = string.Empty;
+    private string _serverName = string.Empty;
+    private string _userName = string.Empty;
+    private string _password = string.Empty;
+    private string _selectedDatabase = string.Empty;
+    private List<string> _databaseList = new List<string>();
+
+    public ConnectionManager()
+    {
+      InitializeComponent();
+    }
+
+    public string ConnectionString
+    {
+      get { return _connectionString; }
+      private set { _connectionString = value; }
+    }
+
+    public string SelectedDatabase
+    {
+      get { return _selectedDatabase; }
+      set { _selectedDatabase = value; }
+    }
+
+    private void radSQLAuthentication_CheckedChanged(object sender, EventArgs e)
+    {
+      var radSQL = sender as RadioButton;
+
+      if (radSQL == null)
+        return;
+
+      var enable = radSQL.Checked;
+      EnableSQLAuthentication(enable);
+
+      btnTestConnection.Enabled = (!string.IsNullOrEmpty(_userName) && !string.IsNullOrEmpty(_password));
+    }
+
+    private void EnableSQLAuthentication(bool enable)
+    {
+      lbUserName.Enabled = enable;
+      lbPassword.Enabled = enable;
+      txtUserName.Enabled = enable;
+      txtPassword.Enabled = enable;
+    }
+
+
+    private void btnOK_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+    private void btnCancel_Click(object sender, EventArgs e)
+    {
+      ConnectionString = string.Empty;
+      this.Close();
+    }
+
+    private void btnTestConnection_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        _serverName = txtServerName.Text;
+        SelectedDatabase = cmbDatabaseList.Text;
+        _userName = txtUserName.Text;
+        _password = txtPassword.Text;
+
+        if (string.IsNullOrEmpty(_serverName) && string.IsNullOrEmpty(SelectedDatabase))
+        {
+          MessageBox.Show(this, Resources.Server_Database_Required, Resources.Test_Connection, MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+          return;
+        }
+
+        if (string.IsNullOrEmpty(_serverName))
+        {
+          MessageBox.Show(this, Resources.Server_Required, Resources.Test_Connection, MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+          return;
+        }
+
+        if (string.IsNullOrEmpty(SelectedDatabase))
+        {
+          MessageBox.Show(this, Resources.Database_Required, Resources.Test_Connection, MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+          return;
+        }
+
+        var connectionString = GetConnectionString();
+        txtConnectionString.Text = connectionString;
+        SQLConnectionManager.TestConnection(connectionString);
+        MessageBox.Show(this, Resources.Connection_Successful, Resources.Test_Connection, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(this, ex.Message, Resources.Test_Connection, MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+
+      }
+
+    }
+
+    private string GetConnectionString()
+    {
+      if (!radSQLAuthentication.Checked)
+      {
+        ConnectionString = string.Format("Server={0};Database={1};Trusted_Connection=True;", _serverName,
+          SelectedDatabase);
+      }
+      else
+      {
+        ConnectionString = string.Format("Server={0};Database={1};User Id={2};Password={3};", _serverName,
+          SelectedDatabase, _userName, _password);
+      }
+
+      return ConnectionString;
+    }
+
+    private void ConnectionManager_Load(object sender, EventArgs e)
+    {
+      _serverName = txtServerName.Text;
+
+      if (string.IsNullOrEmpty(_serverName) || !radWindowsAuthentication.Checked) return;
+
+      var connectionString = GetConnectionString();
+      RefreshDatabaseList(connectionString);
+      GetConnectionString();
+    }
+
+    private void RefreshDatabaseList(string connectionString)
+    {
+      try
+      {
+        _databaseList.Clear();
+        _databaseList = SQLConnectionManager.GetDatabaseList(connectionString);
+        cmbDatabaseList.DataSource = _databaseList;
+      }
+      catch (Exception ex)
+      {
+        Trace.WriteLine(ex);
+      }
+    }
+
+
+
+    private void btnRefreshDatabase_Click(object sender, EventArgs e)
+    {
+      var connectionString = GetConnectionString();
+      RefreshDatabaseList(connectionString);
+    }
+
+    private void txtUserName_TextChanged(object sender, EventArgs e)
+    {
+      var txtUser = sender as TextBox;
+      if (txtUser == null)
+        return;
+
+      btnTestConnection.Enabled = (!string.IsNullOrEmpty(txtUser.Text) && !string.IsNullOrEmpty(txtPassword.Text));
+    }
+
+    private void txtPassword_TextChanged(object sender, EventArgs e)
+    {
+      var txtPass = sender as TextBox;
+      if (txtPass == null)
+        return;
+
+      btnTestConnection.Enabled = (!string.IsNullOrEmpty(txtPass.Text) && !string.IsNullOrEmpty(txtPassword.Text));
+    }
+
+    private void txtConnectionString_TextChanged(object sender, EventArgs e)
+    {
+      var txtConnectionStr = sender as TextBox;
+      if (txtConnectionStr == null)
+        return;
+
+      btnOK.Enabled = !string.IsNullOrEmpty(txtConnectionStr.Text);
+    }
+  }
+}
---
+++ Misc/SQLConnectionManager/SQLConnectionManager/SQLConnectionManager.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLConnectionManager.Properties;
+
+namespace SQLConnectionManager
+{
+  public class SQLConnectionManager
+  {
+
+    public static bool TestConnection(string connectionString)
+    {
+      
+      using (var sqlConnection = new SqlConnection(connectionString))
+      {
+        try
+        {
+          sqlConnection.Open();
+          return true;
+        }
+        catch (SqlException ex)
+        {
+          throw ex;
+        }
+      }
+    }
+
+    public static List<string> GetDatabaseList(string connectionString)
+    {
+      var databaseList = new List<string>();
+      using (var sqlConnection = new SqlConnection(connectionString))
+      {
+        sqlConnection.Open();
+        using (var sqlCommand = new SqlCommand(Resources.SQL_GetDatabases, sqlConnection))
+        {
+          var reader = sqlCommand.ExecuteReader();
+          while (reader.Read())
+          {
+            var databaseName = reader["name"] == DBNull.Value ? string.Empty : reader["name"].ToString();
+            databaseList.Add(databaseName);
+          }
+        }
+
+        sqlConnection.Close();
+      }
+
+      return databaseList;
+    }
+
+    public static List<string> GetTableList(string connectionString)
+    {
+      var tableList = new List<string>();
+      try
+      {
+        using (var sqlConnection = new SqlConnection(connectionString))
+        {
+          sqlConnection.Open();
+          var tableListDataTable = sqlConnection.GetSchema("Tables");
+          var reader = tableListDataTable.CreateDataReader();
+          while (reader.Read())
+          {
+            if (reader[2] == DBNull.Value)
+            {
+              break;
+            }
+
+            tableList.Add(string.Format("[{0}].[{1}]", reader[1].ToString(), reader[2].ToString()));
+          }
+
+          sqlConnection.Close();
+        }
+      }
+      catch (Exception)
+      {
+
+        throw;
+      }
+
+      return tableList;
+    }
+
+    public static List<string> GetViewList(string connectionString)
+    {
+      var viewList = new List<string>();
+      try
+      {
+        using (var sqlConnection = new SqlConnection(connectionString))
+        {
+          sqlConnection.Open();
+          var tableListDataTable = sqlConnection.GetSchema("Views");
+          var reader = tableListDataTable.CreateDataReader();
+          while (reader.Read())
+          {
+            if (reader[2] == DBNull.Value)
+            {
+              break;
+            }
+
+            viewList.Add(string.Format("[{0}].[{1}]", reader[1].ToString(),reader[2].ToString()));
+          }
+          sqlConnection.Close();
+        }
+      }
+      catch (Exception)
+      {
+
+        throw;
+      }
+
+      return viewList;
+    }
+
+    public static DataTable GetTableData(string connectionString, string tableName)
+    {
+      DataTable result = null;
+
+      try
+      {
+        using (var sqlConnection = new SqlConnection(connectionString))
+        {
+          sqlConnection.Open();
+          var command = string.Format("SELECT * FROM {0}", tableName);
+
+          using (var sqlAdapter = new SqlDataAdapter(command, sqlConnection))
+          {
+            result = new DataTable();
+            sqlAdapter.Fill(result);
+          }
+
+          sqlConnection.Close();
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+
+      return result;
+    }
+
+    public static DataTable ExecuteQuery(string connectionString, string query)
+    {
+      DataTable result = null;
+
+      try
+      {
+        using (var sqlConnection = new SqlConnection(connectionString))
+        {
+          sqlConnection.Open();
+          var command = query;
+
+          using (var sqlAdapter = new SqlDataAdapter(command, sqlConnection))
+          {
+            result = new DataTable();
+            sqlAdapter.Fill(result);
+          }
+
+          sqlConnection.Close();
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+
+      return result;
+    }
+
+  }
+}
