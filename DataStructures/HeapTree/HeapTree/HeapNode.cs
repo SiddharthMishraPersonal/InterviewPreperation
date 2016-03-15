b4ce50810bdbae116dd80d3ddc0d4e73ff887b1c
Siddharth Mishra <siddharth82mishra@gmail.com>
2016-03-15 11:23:02 -0600

    Added a new project to create heap without array

---
+++ DataStructures/HeapTree/HeapTree/HeapNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapTree
+{
+    /// <summary>
+    /// The heap node.
+    /// </summary>
+    public class HeapNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HeapNode"/> class.
+        /// </summary>
+        /// <param name="data">
+        /// The data.
+        /// </param>
+        public HeapNode(int data)
+        {
+            this.Data = data;
+            this.ParentNode = null;
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        public int Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the left node.
+        /// </summary>
+        public HeapNode LeftNode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right node.
+        /// </summary>
+        public HeapNode RightNode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent node.
+        /// </summary>
+        public HeapNode ParentNode { get; set; }
+    }
+}
---
+++ DataStructures/HeapTree/HeapTree/MinHeap.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapTree
+{
+    /// <summary>
+    /// The min heap.
+    /// </summary>
+    public class MinHeap
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinHeap"/> class.
+        /// </summary>
+        public MinHeap()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the root node.
+        /// </summary>
+        public HeapNode RootNode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last node.
+        /// </summary>
+        public HeapNode LastNode { get; set; }
+
+        /// <summary>
+        /// The insert.
+        /// </summary>
+        /// <param name="data">
+        /// The data.
+        /// </param>
+        public void Insert(HeapNode rootNode, int data)
+        {
+            var newNode = new HeapNode(data);
+
+            if (this.RootNode == null)
+            {
+                this.RootNode = newNode;
+                this.LastNode = this.RootNode;
+            }
+            else
+            {
+                if (this.LastNode.LeftNode == null)
+                {
+                    this.LastNode.LeftNode = newNode;
+                    return;
+                }
+
+                if (this.LastNode.RightNode == null)
+                {
+                    this.LastNode.RightNode = newNode;
+                    return;
+                }
+
+                this.Insert();
+            }
+        }
+
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        public void Delete()
+        {
+
+        }
+
+        /// <summary>
+        /// Rearranges heap nodes after delete operation.
+        /// </summary>
+        public void Heapify()
+        {
+
+        }
+    }
+}
