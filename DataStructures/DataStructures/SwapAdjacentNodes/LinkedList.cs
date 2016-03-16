1033a08e0cfc3e3a1f90c41266f12b1fb14aa887
Siddharth Mishra <siddharth82mishra@gmail.com>
2016-03-15 23:22:29 -0700

    Fixing a problem statement

---
+++ DataStructures/DataStructures/SwapAdjacentNodes/LinkedList.cs
@@ -0,0 +1,81 @@
+﻿using Sid.Practice.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapAdjacentNodes
+{
+    public class LinkedList
+    {
+        private SinglyNode rootNode;
+
+        public LinkedList()
+        {
+            rootNode = null;
+        }
+
+        public void Insert(int data)
+        {
+            var newNode = new SinglyNode(data);
+            if (rootNode == null)
+            {
+                rootNode = newNode;
+            }
+            else
+            {
+                var currentNode = rootNode;
+                while (currentNode.NextNode != null)
+                {
+                    currentNode = currentNode.NextNode;
+                }
+
+                currentNode.NextNode = newNode;
+            }
+        }
+
+        public void Reverse()
+        {
+            if (rootNode == null)
+            {
+                return;
+            }
+
+            var currentNode = rootNode;
+            var nextNode = currentNode.NextNode;
+            var tempNode = nextNode.NextNode;
+            while (nextNode != null)
+            {
+                tempNode = nextNode.NextNode;
+                nextNode.NextNode = currentNode;
+                currentNode = nextNode;
+                nextNode = tempNode;
+            }
+
+            rootNode.NextNode = null;
+            rootNode = currentNode;
+        }
+
+        public void Swap()
+        {
+
+        }
+
+        public void Display()
+        {
+            if (rootNode == null)
+            {
+                Console.WriteLine("Empty");
+                return;
+            }
+
+            var currentNode = rootNode;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.NodeValue);
+                currentNode = currentNode.NextNode;
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/SwapAdjacentNodes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapAdjacentNodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList();
+            for (int i = 1; i <= 7; i++)
+            {
+                list.Insert(i);
+            }
+
+            list.Display();
+
+            Console.WriteLine("Reverse the list");
+
+            list.Reverse();
+            list.Display();
+
+            Console.ReadKey();
+
+            list.Swap();
+
+        }
+    }
+}
